d0c5d81196f1d7a7e88ed1bcf71d642f3a431448
Pr75502 <pp1614582@gmail.com>
2025-02-21 21:46:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let  age=document.querySelector("#age")
+let name=document.querySelector("#name")
+let btn=document.querySelector("#btn")
+
+
+btn.addEventListener("click", ()=>{
+	if(name.value.trim()==""||age.value.trim()==""){
+		alert("Please enter valid details.")
+		return;
+	}
+
+	
+	promise=new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		
+		if(age.value>18 ){
+			resolve(`Welcome, ${name.value}. You can vote.`)
+		}else{
+			reject(`Oh sorry ${name.value} . You aren't old enough.`)
+			}
+	}, 4000)
+});
+promise.then(message=>alert(message))
+.catch(error=>alert(error))
+
+}
+})
